2b89ec28ddca153da852eddda4a447ab9ae1872c
Beto <betotoro0902@gmail.com>
2020-09-20 18:56:24 -0500

    [UPDATE] Category component with its styles

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Category } from './components/Category/index'
+
+const App = () => {
+  return (
+    <Category />
+  )
+}
+
+export default App
---
+++ src/components/Category/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Anchor = styled.a`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  text-decoration: none;
+  width: 75px;
+`
+
+export const Image = styled.img`
+  height: auto;
+  height: 75px;
+  weight: 75px ;
+  border: 1px solid #ddd;
+  box-shadow: 0px 10px 14px rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  overflow: hidden;
+  object-fit: cover;
+`
+
+
+
