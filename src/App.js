d8e8d77e87c10272ec1d5601768a3b16fc2e0299
Beto <betotoro0902@gmail.com>
2020-09-21 21:06:08 -0500

    [UPDATE] logo svg

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { ListOfCategories } from './components/ListOfCategories/index'
 import { GlobalStyle } from './components/GlobalStyles'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard/index'
 
+import { Logo } from './components/Logo/index'
+
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCard />
     </>
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Svg = styled.svg`
+  width: 350px;
+  margin: -30px 0 0 -30px;
+`
