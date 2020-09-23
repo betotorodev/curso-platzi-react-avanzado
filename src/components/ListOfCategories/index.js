3da15992871bd12d3f25b84c34daa7313575e6c8
Beto <betotoro0902@gmail.com>
2020-09-22 19:18:07 -0500

    [UPDATE] useEffect and useState in listOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category/index'
-import { categories } from '../../../api/db.json'
 
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  
+  useEffect(() => {
+    fetch('https://petgram-backend.betotorodev.vercel.app/categories')
+      .then((response) => response.json())
+      .then(data => setCategories(data))
+  }, [])
+  
   return (
     <List>
       {
