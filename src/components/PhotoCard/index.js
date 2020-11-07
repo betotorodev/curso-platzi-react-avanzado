10a5dbcd432db3f9f5972891a997150bc0757ae6
Beto <betotoro0902@gmail.com>
2020-11-07 07:07:09 -0500

    [FEATURE] implementación de JWT

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -6,7 +6,6 @@ import { FavButton } from '../FavButton/index'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 //utils
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 //css
@@ -14,9 +13,7 @@ import { Article, ImgWrapper, Img } from './styles'
 
 const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [ liked, setLiked ] = useLocalStorage(key, false)
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [ show, element ] = useNearScreen()
 
   return (
@@ -33,12 +30,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             {
               (toggleLike) => {
                 const handleFavClick = () => {
-                  !liked && toggleLike({ 
+                  toggleLike({ 
                     variables: { 
                       input: { id }
                     }
                   })
-                  setLiked(!liked)
                 }
                 return <FavButton liked={liked} likes={likes} onClick={handleFavClick}/>
               }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ import Context from './Context'
 import App from './App'
 
 const client = new ApolloClient({
-  uri: 'https://petgram-backend.betotorodev.vercel.app/graphql'
+  uri: 'https://petgram-backend.betotorodev.vercel.app/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  }
 })
 
 ReactDOM.render(
